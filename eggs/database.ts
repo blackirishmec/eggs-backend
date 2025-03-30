d7f2fb6cc77fbea5a73a1f3ecd43d87464a99d24
Michael Coughlin <blackirishmec@gmail.com>
2025-03-30 15:26:48 -0400

    feat: add Prisma integration and update database schema for egg prices chore: enhance VSCode settings and add .gitignore for environment variables

---
+++ eggs/database.ts
@@ -0,0 +1,20 @@
+import { SQLDatabase } from 'encore.dev/storage/sqldb';
+
+import { PrismaClient } from '@prisma/client';
+
+const DB = new SQLDatabase('eggs', {
+	migrations: {
+		path: './prisma/migrations',
+		source: 'prisma',
+	},
+});
+
+const prisma = new PrismaClient({
+	datasources: {
+		db: {
+			url: DB.connectionString,
+		},
+	},
+});
+
+export { prisma };
---
+++ eggs/encore.service.ts
@@ -0,0 +1,4 @@
+import { Service } from 'encore.dev/service';
+
+// This service allows users to query data related to egg prices in the US.
+export default new Service('eggs');
